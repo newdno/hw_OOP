023f9ed03b3f549a0144ab8df57fd21f9712f73f
test <newdno@yandex.ru>
2024-05-09 17:50:26 +0300

    Ex.3_2

--- hw_oop.py
+++ hw_oop.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -15,16 +16,43 @@ class Student:
         else:
             return 'Ошибка'
         
+    def __str__(self):
+        return (f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self.average_grade()} \nКурсы в процессе изучения: {", ".join(self.courses_in_progress)} \nЗавершенные курсы: {", ".join(self.finished_courses)}')
+        
+    def average_grade(self):
+        av_grade = 0
+        all_grades = []
+        for a in self.grades.values():
+            all_grades.extend(a)
+        if all_grades:
+            av_grade = round(sum(all_grades)/len(all_grades),1)
+        return av_grade
+        
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return(f'Имя: {self.name} \nФамилия: {self.surname}')
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+  
+    def __str__(self):
+        return (f'{super().__str__()} \nСредняя оценка за лекции: {self.average_grade()}')
+   
+    def average_grade(self):
+        av_grade = 0
+        all_grades = []
+        for a in self.grades.values():
+            all_grades.extend(a)
+        if all_grades:
+            av_grade = round(sum(all_grades)/len(all_grades),1)
+        return av_grade
         
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -38,11 +66,16 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+        
+    def __str__(self):
+        return super().__str__()
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['Введение в программирование']
+
  
 some_reviewer = Reviewer('Some', 'Buddy')
 some_reviewer.courses_attached += ['Python']
@@ -52,7 +85,7 @@ some_lecturer.courses_attached += ['Python']
 some_lecturer.courses_attached += ['Git']
  
 some_reviewer.rate_hw(best_student, 'Python', 10)
-some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 7)
 some_reviewer.rate_hw(best_student, 'Python', 10)
 
 best_student.rate_lecturer(some_lecturer, 'Git', 10)
@@ -63,3 +96,6 @@ best_student.rate_lecturer(some_lecturer, 'Git', 10)
  
 print(best_student.grades)
 print(some_lecturer.grades['Git'])
+print(some_reviewer)
+print(some_lecturer)
+print(best_student)
